a047f6bd78f26a472320603adb6cc0734f9a120d
RPelfresne <49885062+RPelfresne@users.noreply.github.com>
2024-06-26 16:52:23 +0200

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,32 @@
 input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
     radio.sendString("un sourire !")
+    Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo1, 0)
+    Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, 50)
+    Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor2, Kitronik_Robotics_Board.MotorDirection.Reverse, 50)
+})
+input.onButtonPressed(Button.AB, function () {
+    Kitronik_Robotics_Board.motorOff(Kitronik_Robotics_Board.Motors.Motor1)
+    Kitronik_Robotics_Board.motorOff(Kitronik_Robotics_Board.Motors.Motor2)
+    Kitronik_Robotics_Board.servoStop(Kitronik_Robotics_Board.Servos.Servo1)
+    Kitronik_Robotics_Board.servoStop(Kitronik_Robotics_Board.Servos.Servo2)
 })
 radio.onReceivedString(function (receivedString) {
     basic.showIcon(IconNames.Happy)
+    if (receivedString == "A") {
+        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, 50)
+        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor2, Kitronik_Robotics_Board.MotorDirection.Reverse, 50)
+    }
+    if (receivedString == "R") {
+        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Reverse, 50)
+        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor2, Kitronik_Robotics_Board.MotorDirection.Forward, 50)
+    }
 })
-input.onGesture(Gesture.Shake, function () {
-    basic.showIcon(IconNames.Chessboard)
+input.onButtonPressed(Button.B, function () {
+    Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Reverse, 50)
+    Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor2, Kitronik_Robotics_Board.MotorDirection.Forward, 50)
+    radio.sendString("un sourire !")
+    basic.clearScreen()
+    Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo2, 180)
 })
-radio.setGroup(2)
+radio.setGroup(1)
