d9581119f7ac9ab0adbc68563e8cb97066cf8752
RPelfresne <49885062+RPelfresne@users.noreply.github.com>
2024-06-26 19:49:12 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,33 +1,31 @@
-input.onButtonPressed(Button.A, function () {
-    basic.clearScreen()
-    radio.sendString("un sourire !")
-    Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo1, 0)
-    Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, 50)
-    Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor2, Kitronik_Robotics_Board.MotorDirection.Reverse, 50)
-})
-input.onButtonPressed(Button.AB, function () {
-    Kitronik_Robotics_Board.motorOff(Kitronik_Robotics_Board.Motors.Motor1)
-    Kitronik_Robotics_Board.motorOff(Kitronik_Robotics_Board.Motors.Motor2)
-    Kitronik_Robotics_Board.servoStop(Kitronik_Robotics_Board.Servos.Servo1)
-    Kitronik_Robotics_Board.servoStop(Kitronik_Robotics_Board.Servos.Servo2)
-})
 radio.onReceivedString(function (receivedString) {
     basic.showIcon(IconNames.Happy)
-    if (receivedString == "A") {
-        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, 20)
-        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor2, Kitronik_Robotics_Board.MotorDirection.Reverse, 20)
+    if (receivedString == "R") {
+        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, 50)
+        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor2, Kitronik_Robotics_Board.MotorDirection.Reverse, 50)
+        Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo1, 0)
+        Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo2, 0)
+        basic.showString("R")
     } else {
-        if (receivedString == "R") {
-            Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Reverse, 20)
-            Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor2, Kitronik_Robotics_Board.MotorDirection.Forward, 20)
+        if (receivedString == "A") {
+            Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Reverse, 50)
+            Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor2, Kitronik_Robotics_Board.MotorDirection.Forward, 50)
+            basic.showString("A")
+            Kitronik_Robotics_Board.servoStop(Kitronik_Robotics_Board.Servos.Servo1)
+            Kitronik_Robotics_Board.servoStop(Kitronik_Robotics_Board.Servos.Servo2)
+        } else {
+            if (receivedString == "S") {
+                Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Reverse, 50)
+                Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor2, Kitronik_Robotics_Board.MotorDirection.Forward, 50)
+                basic.showString("S")
+                Kitronik_Robotics_Board.motorOff(Kitronik_Robotics_Board.Motors.Motor1)
+                Kitronik_Robotics_Board.motorOff(Kitronik_Robotics_Board.Motors.Motor2)
+                Kitronik_Robotics_Board.servoStop(Kitronik_Robotics_Board.Servos.Servo1)
+                Kitronik_Robotics_Board.servoStop(Kitronik_Robotics_Board.Servos.Servo2)
+            } else {
+            	
+            }
         }
     }
-})
-input.onButtonPressed(Button.B, function () {
-    Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Reverse, 50)
-    Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor2, Kitronik_Robotics_Board.MotorDirection.Forward, 50)
-    radio.sendString("un sourire !")
-    basic.clearScreen()
-    Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo2, 180)
 })
 radio.setGroup(1)
